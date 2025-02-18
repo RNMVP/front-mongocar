80552b3adc8721c1a2153b3a0d7deb6377cbb298
RenanNunesViana <renan.devops.viana@gmail.com>
2025-02-18 11:03:24 -0300

    setting up primeng

---
+++ mongocar/src/app/app.config.ts
@@ -0,0 +1,25 @@
+import { ApplicationConfig } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/Aura';
+
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          prefix: 'p',
+          darkModeSelector: 'system',
+          cssLayer: {
+            name: 'primeng',
+            order: 'app-styles, primeng'
+          },
+        }
+      },
+      ripple: true,
+    })
+  ]
+};
--- mongocar/src/app/app.module.ts
+++ mongocar/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {SharedModule} from './shared/shared.module';
+import {appConfig} from './app.config';
 
 @NgModule({
   declarations: [
@@ -12,9 +13,11 @@ import {SharedModule} from './shared/shared.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    ...appConfig.providers,
+  ],
+  bootstrap: [AppComponent,]
 })
 export class AppModule { }
